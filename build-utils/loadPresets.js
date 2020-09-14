74d36e9bb5b9043b74b383982959f104681656d3
Nicolás Tinte <nico.tinte@gmail.com>
2020-09-14 20:06:04 -0300

    Initial commit

---
+++ build-utils/loadPresets.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line node/no-unpublished-require
+const webpackMerge = require('webpack-merge');
+
+const applyPresets = (env = { presets: [] }) => {
+  const presets = env.presets || [];
+
+  const mergedPresets = [].concat(...[presets]);
+  // eslint-disable-next-line global-require
+  const mergedConfigs = mergedPresets.map((presetName) => require(`./presets/webpack.${presetName}`)(env));
+
+  return webpackMerge({}, ...mergedConfigs);
+};
+
+module.exports = applyPresets;
