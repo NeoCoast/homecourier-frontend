bdb633b31cf457ba16e33513dcab54114cf3795f
manurodriguezz <manu.rodriguezz@hotmail.com>
2020-10-13 00:38:23 -0300

    fix en RatingContainer

--- src/containers/RateOrder/index.jsx
+++ src/containers/RateOrder/index.jsx
@@ -8,9 +8,17 @@ import PropTypes from 'prop-types';
 const RateOrder = (props) => {
   const {
     orderId,
+    stars,
+    title,
+    description,
+    buttonLabel,
+    errorMessageComment,
+    errorMessageRating,
+    successMessage,
     show,
     setShow,
   } = props;
+
   const isHelpee = (undefined === useSelector((state) => state.logUser.document_number));
 
   const rate = (info) => {
@@ -23,7 +31,18 @@ const RateOrder = (props) => {
 
   return (
     <Box>
-      <Rating onSubmit={rate} show={show} setShow={setShow} />
+      <Rating
+        onSubmit={rate}
+        show={show}
+        setShow={setShow}
+        stars={stars}
+        title={title}
+        description={description}
+        buttonLabel={buttonLabel}
+        errorMessageComment={errorMessageComment}
+        errorMessageRating={errorMessageRating}
+        successMessage={successMessage}
+      />
     </Box>
   );
 };
@@ -32,6 +51,23 @@ RateOrder.propTypes = {
   orderId: PropTypes.number.isRequired,
   show: PropTypes.bool.isRequired,
   setShow: PropTypes.func.isRequired,
+  stars: PropTypes.number,
+  title: PropTypes.string,
+  description: PropTypes.string,
+  buttonLabel: PropTypes.string,
+  errorMessageComment: PropTypes.string,
+  errorMessageRating: PropTypes.string,
+  successMessage: PropTypes.string,
+};
+
+RateOrder.defaultProps = {
+  stars: 5,
+  title: 'Califique al voluntario',
+  description: 'Por favor, haga un comentario sobre su experiencia',
+  buttonLabel: 'Calificar',
+  errorMessageRating: 'Debe calificar al voluntario antes de continuar.',
+  errorMessageComment: 'Por favor, de un comentario sobre que no fue de su agrado en su experiencia',
+  successMessage: 'Ha calificado con éxito. Gracias!',
 };
 
 export default RateOrder;
