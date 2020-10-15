79bdff0c5261297d4cd93452614955ec15cdcf10
manurodriguezz <manu.rodriguezz@hotmail.com>
2020-10-15 15:07:33 -0300

    Added Tests

--- src/containers/Profile/index.jsx
+++ src/containers/Profile/index.jsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.scss';
 import { Box, Heading } from 'grommet';
 import { useSelector } from 'react-redux';
+import RateOrder from 'Containers/RateOrder';
 
 const Profile = () => {
   const userInfo = useSelector((state) => state.logUser.data);
+  const [unrated, setUnrated] = useState(true);
 
   return (
     <Box fill align="center">
       <Box>
         <Heading>Bienvenido {userInfo.username}</Heading>
+        <RateOrder orderId={4} show={unrated} setShow={setUnrated} />
       </Box>
     </Box>
   );
