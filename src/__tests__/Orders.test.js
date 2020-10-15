2f553dfd0d1a8d787c75150e467f094a2fe29711
agus-ameig <39110433+agus-ameig@users.noreply.github.com>
2020-10-15 19:15:17 -0300

    Update src/__tests__/Orders.test.js
    Co-authored-by: Agustín Bologna <71099874+Etzior@users.noreply.github.com>

--- src/__tests__/Orders.test.js
+++ src/__tests__/Orders.test.js
@@ -47,7 +47,7 @@ describe('Orders', () => {
   beforeEach(() => {
     useSelector.mockImplementation((selector) => selector({
       logUser: {
-        data: { documentNumber: '232323', name: 'A', lastName: 'A' },
+        data: { documentNumber: '232323', name: faker.name.firstName(), lastName: faker.name.lastName() },
         loggedIn: false,
       },
     }));
