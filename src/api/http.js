0c27e30edff6b8e05dfed8ca918abf8299c0826a
Andrés Collares <andres.collares@fing.edu.uy>
2020-10-17 16:57:07 -0300

    set Access-Control-Allow-Credentials header

--- src/api/http.js
+++ src/api/http.js
@@ -9,6 +9,7 @@ const HTTP = applyConverters(
       Accept: 'application/json',
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
     },
   }),
 );
