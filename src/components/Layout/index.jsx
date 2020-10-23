f931f87593fa2d951a4a3924947d3d15c9d87981
Diego Wins <diegow93@gmail.com>
2020-10-23 16:31:51 -0300

    Fixing UI locking.

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,37 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import {
   Header, Grommet, Main, Grid,
 } from 'grommet';
 import theme from 'Theme/theme';
 import CustomToast from 'Components/CustomToast';
-
 import Background from 'Assets/background.svg';
-
 import AppBar from 'Components/AppBar';
+import { useSelector } from 'react-redux';
+import RateOrder from 'Containers/RateOrder';
 
-const Layout = ({ children }) => (
-  <Grommet theme={theme} full>
-    <Grid
-      rows={['72px', 'flex']}
-      columns={['flex']}
-      fill="vertical"
-      areas={[
-        { name: 'header', start: [0, 0], end: [0, 0] },
+const Layout = ({ children }) => {
+  const userInfo = useSelector((state) => state.logUser);
+  const [rate, setRate] = useState(false);
 
-        { name: 'main', start: [0, 1], end: [0, 1] },
-      ]}
-    >
-      <Header background="white" gridArea="header" elevation="small" gap="none">
-        <AppBar />
-        <CustomToast />
-      </Header>
-      <Main gridArea="main" background={`url(${Background})`}>
-        {children}
-      </Main>
-    </Grid>
-  </Grommet>
-);
+  useEffect(() => {
+    setRate((userInfo.data) && (userInfo.data.pendingRate));
+  }, [userInfo]);
+
+  return (
+    <Grommet theme={theme} full>
+      <Grid
+        rows={['72px', 'flex']}
+        columns={['flex']}
+        fill="vertical"
+        areas={[
+          { name: 'header', start: [0, 0], end: [0, 0] },
+
+          { name: 'main', start: [0, 1], end: [0, 1] },
+        ]}
+      >
+        <Header background="white" gridArea="header" elevation="small" gap="none">
+          <AppBar />
+          <CustomToast />
+        </Header>
+        <Main gridArea="main" background={`url(${Background})`}>
+          {children}
+          {rate && <RateOrder orderId={userInfo.data.pendingRateId} setShow={setRate} show={rate} />}
+        </Main>
+      </Grid>
+    </Grommet>
+  );
+};
 
 Layout.propTypes = {
   children: PropTypes.node,
