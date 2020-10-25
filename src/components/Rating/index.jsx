5631dd286cdb95a2b68d9164c91fe35691582ded
manurodriguezz <manu.rodriguezz@hotmail.com>
2020-10-24 23:45:13 -0300

    Fixes en Rating responsive

--- src/components/Rating/index.jsx
+++ src/components/Rating/index.jsx
@@ -1,14 +1,14 @@
 /* eslint-disable no-unused-expressions */
 /* eslint-disable no-plusplus */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
-  Box, Layer, Button, TextArea, Heading, Stack,
+  Box, Layer, Button, TextArea, Heading, Stack, ResponsiveContext, Form, FormField, Text,
 } from 'grommet';
 import { StatusGoodSmall, Fireball } from 'grommet-icons';
 import Spinner from 'Components/Utils/Spinner';
 import PropTypes from 'prop-types';
-import ErrorModal from 'Components/Modals/ErrorModal';
 import SuccessModal from 'Components/Modals/SuccessModal';
+import { validateComment } from 'Helpers/validator.helper';
 
 const Rating = (props) => {
   const {
@@ -18,7 +18,6 @@ const Rating = (props) => {
     buttonLabel,
     onSubmit,
     errorMessageComment,
-    errorMessageRating,
     successMessage,
     setShow,
     show,
@@ -29,32 +28,42 @@ const Rating = (props) => {
   const [active, setActive] = useState(-1);
   const [feedBack, setFeedBack] = useState('');
   const [loading, setLoading] = useState(false);
-  const [invalid, setInvalid] = useState(false);
-  const [error, setError] = useState('');
   const [success, setSuccess] = useState(false);
+  const gapSize = React.useContext(ResponsiveContext);
+  const [iconSize, setIconSize] = useState('large');
+  const [headingSize, setHeadingSize] = useState(2);
+
+  const errorMessage = () => (
+    <Text size="small" color="red">
+      {errorMessageComment}
+    </Text>
+  );
+
+  useEffect(() => {
+    if (gapSize === 'small') {
+      setIconSize('medium');
+      setHeadingSize(5);
+    } else {
+      setIconSize('large');
+      setHeadingSize(2);
+    }
+  }, [gapSize]);
 
   const handleSubmit = async () => {
-    if (active < 0 || (active < stars / 2 && !feedBack)) {
-      setInvalid(true);
-      active < 0 ? setError(errorMessageRating) : setError(errorMessageComment);
-    } else {
-      setLoading(true);
-      const info = {
-        score: active,
-        comment: feedBack,
-      };
-      try {
-        await onSubmit(info);
-        setLoading(false);
-        setActive(-1);
-        setShow(false);
-        setSuccess(true);
-      } catch (failure) {
-        setLoading(false);
-        setError(failure);
-        setInvalid(true);
-        console.log(failure);
-      }
+    setLoading(true);
+    const info = {
+      score: active,
+      comment: feedBack,
+    };
+    try {
+      await onSubmit(info);
+      setLoading(false);
+      setActive(-1);
+      setShow(false);
+      setSuccess(true);
+    } catch (failure) {
+      setLoading(false);
+      console.log(failure);
     }
   };
 
@@ -67,49 +76,61 @@ const Rating = (props) => {
       >
         {active >= i && (
           <StatusGoodSmall
-            size="large"
+            size={iconSize}
             color="#f2da81"
           />
         )}
         <Fireball
-          size="large"
+          size={iconSize}
           color="black"
         />
       </Stack>
     );
   }
   return (
-    <Box>
+    <Box responsive={false}>
       {loading && <Spinner />}
-      {invalid && <ErrorModal errorMessage={error} setShow={setInvalid} show={invalid} />}
       {success && <SuccessModal message={successMessage} setShow={setSuccess} show={success} />}
-      { show && !invalid
+      { show
           && (
             <Layer margin="large" position="center" modal responsive={false}>
-              <Heading margin="small" level={2} alignSelf="center">{ title }</Heading>
+              <Heading margin="small" level={headingSize} alignSelf="center">{ title }</Heading>
               <Box
                 justify="center"
                 alignSelf="center"
                 direction="row"
-                gap="medium"
-                margin="small"
+                gap={gapSize}
+                margin="xsmall"
+                responsive={false}
               >
                 { starsElement }
               </Box>
-              <Box align="center" pad="medium">
-                <TextArea
-                  size="medium"
-                  id="Comment"
-                  placeholder={description}
-                  onChange={(event) => setFeedBack(event.target.value)}
-                />
-              </Box>
-              <Button
-                margin="small"
-                onClick={handleSubmit}
-                label={buttonLabel}
-              >
-              </Button>
+              <Form onSubmit={handleSubmit}>
+                <Box pad="small">
+                  <FormField name="Comment" validate={(value) => validateComment(value, (active >= Math.floor(stars / 2)), errorMessage)}>
+                    <Box align="center" height="small">
+                      <TextArea
+                        name="Comment"
+                        id="Comment"
+                        size="medium"
+                        resize={false}
+                        fill
+                        placeholder={description}
+                        onChange={(event) => setFeedBack(event.target.value)}
+                      />
+                    </Box>
+                  </FormField>
+                </Box>
+                <Box pad="small">
+                  <Button
+                    primary
+                    disabled={active < 0}
+                    fill="horizontal"
+                    label={buttonLabel}
+                    type="submit"
+                  />
+                </Box>
+              </Form>
             </Layer>
           )}
     </Box>
@@ -122,7 +143,6 @@ Rating.propTypes = {
   description: PropTypes.string,
   buttonLabel: PropTypes.string,
   errorMessageComment: PropTypes.string,
-  errorMessageRating: PropTypes.string,
   onSubmit: PropTypes.func.isRequired,
   successMessage: PropTypes.string,
   show: PropTypes.bool.isRequired,
@@ -134,7 +154,6 @@ Rating.defaultProps = {
   title: 'Califique al voluntario',
   description: 'Por favor, haga un comentario sobre su experiencia',
   buttonLabel: 'Calificar',
-  errorMessageRating: 'Debe calificar al voluntario antes de continuar.',
   errorMessageComment: 'Por favor, de un comentario sobre que no fue de su agrado en su experiencia',
   successMessage: 'Ha calificado con éxito. Gracias!',
 };
--- src/containers/Profile/index.jsx
+++ src/containers/Profile/index.jsx
@@ -1,27 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './index.scss';
 import { Box, Heading } from 'grommet';
 import { useSelector } from 'react-redux';
-import RateOrder from 'Containers/RateOrder';
 
 const Profile = () => {
   const userInfo = useSelector((state) => state.logUser.data);
-  const [rate, setRate] = useState(false);
-
-  useEffect(() => {
-    if (userInfo.pendingRate) {
-      setRate(true);
-    } else {
-      setRate(false);
-    }
-  }, [userInfo]);
-
   return (
     <Box fill align="center">
       <Box>
         <Heading>Bienvenido {userInfo.username}</Heading>
       </Box>
-      <RateOrder orderId={userInfo.pendingRateId} setShow={setRate} show={rate} />
     </Box>
   );
 };
