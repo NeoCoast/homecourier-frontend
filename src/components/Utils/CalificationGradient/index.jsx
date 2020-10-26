a90c542897ad42292df3b21912fad205b8520acb
agus-ameig <agus.ameig@gmail.com>
2020-10-26 20:30:31 -0300

    added rounding to gradient

--- src/components/Utils/CalificationGradient/index.jsx
+++ src/components/Utils/CalificationGradient/index.jsx
@@ -7,7 +7,7 @@ import PropTypes from 'prop-types';
 const CalificationGradient = ({ percent, maxRating }) => (
   <Box pad={{ vertical: 'small' }}>
     <ProgressBar percent={percent} filledBackground="linear-gradient(to right, #EF626C, #77dd77)" width="50" />
-    <Text size="small">{(percent * maxRating) / 100}/{maxRating}</Text>
+    <Text size="small">{((percent * maxRating) / 100).toFixed(1)}/{maxRating.toFixed(1)}</Text>
   </Box>
 );
 
