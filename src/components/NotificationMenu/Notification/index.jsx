ba0ab87351340d0e107a8c866e4a10c851feaa40
agus-ameig <agus.ameig@gmail.com>
2020-10-28 16:48:20 -0300

    added multiple visual fixes

--- src/components/NotificationMenu/Notification/index.jsx
+++ src/components/NotificationMenu/Notification/index.jsx
@@ -4,10 +4,12 @@ import { dateToPrint } from 'Helpers/utils.helper';
 
 const Notification = (item) => (
   <Box direction="row" justify="between" fill="horizontal" align="center" gap="small" style={{ minHeight: '50px' }} key={item.id}>
-    {item.status === 'not_seen' && <Box round background="brand" style={{ height: '10px', width: '10px' }} id="new-box"></Box>}
-    <Box alignSelf="start" style={{ maxWidth: '400px' }}>
-      <Text truncate style={{ fontWeight: 'bold' }}>{item.title}</Text>
-      <Text truncate>{item.body}</Text>
+    <Box align="center" direction="row" gap="small">
+      {item.status === 'not_seen' && <Box round background="brand" style={{ height: '10px', width: '10px' }} id="new-box"></Box>}
+      <Box style={{ maxWidth: '400px' }}>
+        <Text truncate style={{ fontWeight: 'bold' }}>{item.title}</Text>
+        <Text truncate>{item.body}</Text>
+      </Box>
     </Box>
     <Box>
       <Text size="small">{dateToPrint(item.createdAt)}</Text>
