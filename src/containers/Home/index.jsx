ce3372dd3aa7b4064d3d2ca2a2679cc71dfd43c3
Agustin Gazzano <agustingazzano107@gmail.com>
2020-10-28 19:36:15 -0300

    removed lorem ipsum from homepage

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './index.scss';
-import { Box, Heading, Markdown } from 'grommet';
+import { Box, Heading } from 'grommet';
 
 const Home = () => (
   <Box fill align="center">
     <Heading>Bienvenido!</Heading>
-    <Markdown>## Lorem ipsum, [link](https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=PLj65iUKyj0nV8hP7hjZ9-XDJ3dpL4LRmm)</Markdown>
   </Box>
 );
 
