536b64c0c5c21cbe2125fd97c0968dd3578cc3c7
manurodriguezz <manu.rodriguezz@hotmail.com>
2020-10-31 16:02:50 -0300

    Added username prop to Rating component and RateOrder

--- src/containers/RateOrder/index.jsx
+++ src/containers/RateOrder/index.jsx
@@ -16,6 +16,7 @@ const RateOrder = (props) => {
     successMessage,
     show,
     setShow,
+    username,
   } = props;
   const dispatch = useDispatch();
   const isHelpee = undefined === useSelector((state) => state.logUser.data.documentNumber);
@@ -50,6 +51,7 @@ const RateOrder = (props) => {
           errorMessageComment={errorMessageComment}
           errorMessageRating="Debe calificar al voluntario antes de continuar."
           successMessage={successMessage}
+          username={username}
         />
       )} {console.log('errorMessageComment: ', errorMessageComment)}
       {!isHelpee && (
@@ -64,6 +66,7 @@ const RateOrder = (props) => {
           errorMessageComment={errorMessageComment}
           errorMessageRating="Debe calificar al usuario antes de continuar."
           successMessage={successMessage}
+          username={username}
         />
       )}
     </Box>
@@ -79,6 +82,7 @@ RateOrder.propTypes = {
   buttonLabel: PropTypes.string,
   errorMessageComment: PropTypes.string,
   successMessage: PropTypes.string,
+  username: PropTypes.string,
 };
 
 RateOrder.defaultProps = {
@@ -87,6 +91,7 @@ RateOrder.defaultProps = {
   buttonLabel: 'Calificar',
   errorMessageComment: 'Por favor, de un comentario sobre que no fue de su agrado en su experiencia',
   successMessage: 'Ha calificado con éxito. ¡Gracias!',
+  username: '',
 };
 
 export default RateOrder;
