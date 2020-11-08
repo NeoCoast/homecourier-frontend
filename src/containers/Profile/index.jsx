1f76981111e8fa266cec9994920a1874fb1d4da2
manurodriguezz <manu.rodriguezz@hotmail.com>
2020-11-08 15:43:23 -0300

    Added exactLocation component, and location of user in profile

--- src/containers/Profile/index.jsx
+++ src/containers/Profile/index.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './index.scss';
 import { Box, Heading } from 'grommet';
 import { useSelector } from 'react-redux';
+import ExactLocation from 'Components/MapsExactLocation';
 
 const Profile = () => {
   const userInfo = useSelector((state) => state.logUser.data);
+  useEffect(() => {
+    console.log(userInfo);
+  });
   return (
     <Box fill align="center">
-      <Box>
-        <Heading>Bienvenido {userInfo.username}</Heading>
+      <Box overflow="auto" justify="stretch">
+        <Heading alignSelf="stretch">Perfil de {userInfo.username}</Heading>
+        <Box margin="small" overflow="auto">
+          <ExactLocation isMarkerShown lat={userInfo.latitude} lng={userInfo.longitude} zoom={16} size={300} />
+        </Box>
       </Box>
     </Box>
   );
