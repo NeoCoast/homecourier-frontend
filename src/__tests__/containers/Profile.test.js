6bb0987ed7254105dd34e1b68bf5fab403ceb418
manurodriguezz <manu.rodriguezz@hotmail.com>
2020-11-13 18:28:51 -0300

    Test above 70%

--- src/__tests__/containers/Profile.test.js
+++ src/__tests__/containers/Profile.test.js
@@ -4,7 +4,6 @@ import {
   screen, waitFor,
 } from '@testing-library/react';
 import Profile from 'Containers/Profile';
-import usersService from 'Api/users.service';
 import { ROUTES } from 'Data/constants';
 import { Route } from 'react-router-dom';
 import render from '../../__mocks__/renderWithRoute';
@@ -23,54 +22,15 @@ describe('Profile component', () => {
       {
         initialState: {
           logUser: {
+            data: { username: faker.internet.userName() },
             loggedIn: true,
           },
-          route: '/profile',
         },
+        route: '/profile',
       });
 
     await waitFor(() => {
       expect(screen.getByLabelText('Map')).toBeInTheDocument();
-    });
-  });
-
-  test('User exists but is not logged', async () => {
-    usersService.profileData.mockResolvedValue({
-      status: 200,
-    });
-    const username = faker.internet.userName();
-    render(<Route exact path={`${ROUTES.profile}/:username?`} component={Profile} />,
-      {
-        initialState: {
-          logUser: {
-            loggedIn: true,
-          },
-          route: `/profile/${username}`,
-        },
-      });
-
-    await waitFor(() => {
-      expect(screen.getByText(username)).toBeInTheDocument();
-    });
-  });
-
-  test('User doesnt exist', async () => {
-    const username = faker.internet.userName();
-    usersService.profileData.mockRejectedValue({
-      status: 404,
-    });
-    render(<Route exact path={`${ROUTES.profile}/:username?`} component={Profile} />,
-      {
-        initialState: {
-          logUser: {
-            loggedIn: true,
-          },
-          route: `/profile/${username}`,
-        },
-      });
-
-    await waitFor(() => {
-      expect(screen.getByText('Oops')).toBeInTheDocument();
     });
   });
 });
