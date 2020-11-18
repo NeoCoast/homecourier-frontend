d90a946e1674ac01c3a0aa5cee21cca4e41a1b01
Agustin Gazzano <agustingazzano107@gmail.com>
2020-11-18 20:09:07 -0300

    added more tests

--- src/__tests__/containers/Register.test.js
+++ src/__tests__/containers/Register.test.js
@@ -53,6 +53,7 @@ describe('Register container', () => {
     const birthDayValue = faker.random.number(30);
     const month = MONTHS[9];
     const birthYearValue = faker.random.number({ min: 1, max: 4 });
+    const phoneValue = `09${faker.random.number({ min: 1000000, max: 9999999 })}`;
 
     // select month
     fireEvent.click(screen.getByLabelText('birthMonth'));
@@ -68,6 +69,7 @@ describe('Register container', () => {
     fireEvent.change(screen.getByLabelText('address'), { target: { value: addressValue } });
     fireEvent.change(screen.getByLabelText('birthDay'), { target: { value: birthDayValue } });
     fireEvent.change(screen.getByLabelText('birthYear'), { target: { value: birthYearValue } });
+    fireEvent.change(screen.getByLabelText('phone'), { target: { value: phoneValue } });
 
     expect(screen.getByRole('form')).toBeInTheDocument();
 
@@ -85,6 +87,58 @@ describe('Register container', () => {
         'Su solicitud de registro se ha enviado correctamente, '
         + 'le hemos enviado un mail de confirmación a su casilla de correo.'
       )).toBeInTheDocument();
+    });
+  });
+
+  test('Validates password and repeat password fields', async () => {
+    render(<Register />);
+
+    const passwordValue = faker.random.number();
+    const repeatPasswordValue = passwordValue + 1;
+
+    fireEvent.change(screen.getByLabelText('password'), { target: { value: passwordValue } });
+    fireEvent.change(screen.getByLabelText('repeatPassword'), { target: { value: repeatPasswordValue } });
+
+    expect(screen.getByRole('form')).toBeInTheDocument();
+
+    const usernameResponse = faker.internet.userName();
+    helpeeService.create.mockResolvedValue({
+      status: 200,
+      username: usernameResponse,
+    });
+
+    // submit form
+    fireEvent.click(screen.getByRole('button', { name: 'Registrarse' }));
+
+    await waitFor(() => {
+      expect(screen.getByText('Las contraseñas no coinciden')).toBeInTheDocument();
+    });
+  });
+
+  test('Validates date fields', async () => {
+    render(<Register />);
+    const birthDayValue = faker.random.number({ min: 32, max: 99 });
+    const month = MONTHS[9];
+    const birthYearValue = faker.random.words();
+
+    fireEvent.click(screen.getByLabelText('birthMonth'));
+    fireEvent.click(screen.getByText(month.displayValue));
+    fireEvent.change(screen.getByLabelText('birthDay'), { target: { value: birthDayValue } });
+    fireEvent.change(screen.getByLabelText('birthYear'), { target: { value: birthYearValue } });
+
+    expect(screen.getByRole('form')).toBeInTheDocument();
+
+    const usernameResponse = faker.internet.userName();
+    helpeeService.create.mockResolvedValue({
+      status: 200,
+      username: usernameResponse,
+    });
+
+    // submit form
+    fireEvent.click(screen.getByRole('button', { name: 'Registrarse' }));
+
+    await waitFor(() => {
+      expect(screen.getAllByText(/No válido/i)).toHaveLength(3);
     });
   });
 
@@ -128,7 +182,7 @@ describe('Register container', () => {
     fireEvent.click(screen.getByRole('button', { name: 'Registrarse' }));
 
     await waitFor(() => {
-      expect(screen.getByText('Verifique que los datos introducidos sean correctos')).toBeInTheDocument();
+      expect(screen.getByText('No válido')).toBeInTheDocument();
     });
   });
 });
