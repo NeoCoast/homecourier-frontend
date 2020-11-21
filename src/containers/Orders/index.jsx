12b62799e3f36310aa788a99d90c9137484aacd1
Martin <martinrotti.b@gmail.com>
2020-11-21 12:32:48 -0300

    orders ordered by distance and antiquity

--- src/containers/Orders/index.jsx
+++ src/containers/Orders/index.jsx
@@ -3,11 +3,13 @@ import { Box, Heading } from 'grommet';
 import ordersService from 'Api/orders.service';
 import OrdersList from 'Components/OrdersList';
 import Spinner from 'Components/Utils/Spinner';
+import { useSelector } from 'react-redux';
 
 const Orders = () => {
   const [orders, setOrders] = useState([]);
   const [loading, setLoading] = useState(false);
   const [viewOrderModal, setViewOrderModal] = useState(true);
+  const userData = useSelector((state) => state.logUser.data);
 
   useEffect(() => {
     const fetchOrders = async () => {
@@ -25,6 +27,51 @@ const Orders = () => {
     if (viewOrderModal) fetchOrders();
   }, [viewOrderModal]);
 
+  const ordersForDistance = async (ascDesc) => {
+    try {
+      setLoading(true);
+      const res = await ordersService.ordersForDistance(userData.id, ascDesc);
+      setOrders(res.data);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+    }
+  };
+
+  const ordersForAntiquity = async (ascDesc) => {
+    try {
+      setLoading(true);
+      const res = await ordersService.ordersForAntiquity('created', ascDesc);
+      setOrders(res.data);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+    }
+  };
+
+  const filter = (option) => {
+    switch (option) {
+      case 'Distancia descendente': {
+        ordersForDistance('desc');
+        break;
+      }
+      case 'Distancia ascendente': {
+        ordersForDistance('asc');
+        break;
+      }
+      case 'Pedidos más nuevos': {
+        ordersForAntiquity('desc');
+        break;
+      }
+      case 'Pedidos más antiguos': {
+        ordersForAntiquity('asc');
+        break;
+      }
+      default:
+        break;
+    }
+  };
+
   return (
     <Box id="box" fill align="center">
       {loading && <Spinner />}
@@ -33,7 +80,7 @@ const Orders = () => {
           ¡Lo sentimos! No hay pedidos en el sistema.
         </Heading>
       )}
-      {orders.length > 0 && <OrdersList orders={orders} setLoading={setLoading} modalClosed={setViewOrderModal} />}
+      {orders.length > 0 && <OrdersList orders={orders} setLoading={setLoading} modalClosed={setViewOrderModal} onChange={filter} />}
     </Box>
   );
 };
